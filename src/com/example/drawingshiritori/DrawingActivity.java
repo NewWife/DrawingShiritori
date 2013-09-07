ab486903cedcdd28dc714091dfbffec6b407705e
Hiromichi Otsuka <hiro2220@live.jp>
2013-09-07 19:56:22 +0900

    test

--- src/com/example/drawingshiritori/DrawingActivity.java
+++ src/com/example/drawingshiritori/DrawingActivity.java
@@ -6,6 +6,10 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+<<<<<<< HEAD
+=======
+import android.os.CountDownTimer;
+>>>>>>> devel
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -13,14 +17,55 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+<<<<<<< HEAD
+=======
+import android.widget.Toast;
+>>>>>>> devel
 
 public class DrawingActivity extends Activity
 implements OnClickListener
 {
+<<<<<<< HEAD
 	// 【DEBUG】
 	private final String LOG = "DrawingActivity";
 	// 【DEBUG】テンプレートのお題をこちらで決める
 	private final String[] TEMPLATE_THEME_WORD = {"とけい", "ゴリラ", "ラッパ", "コップ", "ハンガー"};
+=======
+	public class MyCountDownTimer extends CountDownTimer
+	{
+		private TextView timerView;
+
+		public MyCountDownTimer(long millisInFuture, long countDownInterval, TextView timerView)
+		{
+			super(millisInFuture, countDownInterval);
+			this.timerView = timerView;
+		}
+
+		@Override
+		public void onFinish()
+		{
+			// カウントダウン完了後に呼ばれる
+			timerView.setText("0.00");
+			// 現在描いている人を罰ゲームの対象にして罰ゲーム表示画面へ
+			globals.pather.add(globals.now);
+			Intent intent = new Intent(DrawingActivity.this, ShowingPenaltyActivity.class);
+			startActivity(intent);
+		}
+
+		@Override
+		public void onTick(long millisUntilFinished) {
+
+			// インターバル(countDownInterval)毎に呼ばれる
+			timerView.setText(Long.toString(millisUntilFinished/1000%60) + ":" + String.format("%02d", (millisUntilFinished / 10)%100));
+		}
+
+	}
+
+	// 【DEBUG】
+	private final String LOG = "DrawingActivity";
+	// 【DEBUG】テンプレートのお題をこちらで決める
+	private final String[] TEMPLATE_THEME_WORD = {"とけい", "ごりら", "らっぱ", "こっぷ", "はんが"};
+>>>>>>> devel
 	// 【DEBUG】制限時間をこちらで決める
 	private final int LIMIT_TIME = 60;
 
@@ -29,17 +74,40 @@ implements OnClickListener
 
 	// お絵かきビュー
 	private DrawSurfaceView drawSurfaceView;
+<<<<<<< HEAD
 	private boolean isFirst;
 	private boolean isInputedWord;
 	private String themeWord;
+=======
+	// カウントダウンをするカスタムクラス
+	private EditText wordEditText;
+	private MyCountDownTimer myCountDownTimer;
+	private boolean isFirst;
+	private boolean isInputedWord;
+
+	// お題の格納する変数
+	private String preWord;
+
+	private Globals globals;
+>>>>>>> devel
 
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.drawing);
 
+<<<<<<< HEAD
 		// get layout content
 		drawSurfaceView = (DrawSurfaceView)findViewById(R.id.draw_surface_view);
+=======
+		globals = (Globals)this.getApplication();
+
+		// get layout content
+		wordEditText = (EditText)findViewById(R.id.drawing_word_edit_text);
+		drawSurfaceView = (DrawSurfaceView)findViewById(R.id.draw_surface_view);
+		drawSurfaceView.disableDrawing();
+
+>>>>>>> devel
 		Button okButton = (Button)findViewById(R.id.drawing_word_ok_button);
 		okButton.setOnClickListener(this);
 		Button nextButton = (Button)findViewById(R.id.drawing_next_button);
@@ -50,11 +118,19 @@ implements OnClickListener
 		eraserButton.setOnClickListener(this);
 		ImageButton penButton = (ImageButton)findViewById(R.id.drawing_pen_button);
 		penButton.setOnClickListener(this);
+<<<<<<< HEAD
+=======
+		TextView remainTextView = (TextView)findViewById(R.id.drawing_remain_time_text_view);
+
+		// カウントダウンタイマーを指定の時間で初期化する
+		myCountDownTimer = new MyCountDownTimer(LIMIT_TIME * 1000, 10, remainTextView);
+>>>>>>> devel
 
 		// 一番始めかどうかを調べる
 		Intent intent = getIntent();
 		isFirst = intent.getBooleanExtra("isFirst", false);
 
+<<<<<<< HEAD
 		// 始めならば
 		if(isFirst)
 		{
@@ -67,6 +143,15 @@ implements OnClickListener
 			editText.setText(themeWord);
 			editText.setEnabled(false);
 			editText.setFocusable(false);
+=======
+		// 始めてならば
+		if(isFirst)
+		{
+			// テーマを自動的に生成する
+			preWord = generateThemeWord();
+			// 単語入力の無効化を行う
+			disableWordInput();
+>>>>>>> devel
 		}
 	}
 
@@ -78,13 +163,21 @@ implements OnClickListener
 		// 単語の入力欄に対してOKを押した場合
 		case R.id.drawing_word_ok_button:
 		{
+<<<<<<< HEAD
 
+=======
+			onClickOkButton();
+>>>>>>> devel
 			break;
 		}
 		// 次の画面へ移動する
 		case R.id.drawing_next_button:
 		{
+<<<<<<< HEAD
 
+=======
+			onClickNextButton();
+>>>>>>> devel
 			break;
 		}
 		// クリアボタンが押された場合
@@ -97,7 +190,10 @@ implements OnClickListener
 		case R.id.drawing_eraser_button:
 		{
 			drawSurfaceView.setColor(Color.WHITE, ERASER_WIDTH);
+<<<<<<< HEAD
 			Log.d(LOG, "push erase button");
+=======
+>>>>>>> devel
 			break;
 		}
 		// 鉛筆ボタンが押された場合
@@ -111,9 +207,99 @@ implements OnClickListener
 		}
 	}
 
+<<<<<<< HEAD
 	private void onClickOkButton()
 	{
 
+=======
+	/**
+	 * 書くお題に対してOKボタンを押した時の
+	 */
+	private void onClickOkButton()
+	{
+		String inputWord = wordEditText.getText().toString();
+
+		if(!validation(inputWord)) return;
+		if(!isInputedWord)
+		{
+			myCountDownTimer.start();
+			disableWordInput();
+			disableOkButton();
+			drawSurfaceView.enableDrawing();
+			Toast.makeText(this, "start drawing", Toast.LENGTH_SHORT).show();
+			isInputedWord = true;
+		}
+		// 外した場合
+		/*
+		else
+		{
+			// 順番を次に飛ばして
+			int next = (++globals.now) % globals.player;
+			// 次のプレイヤーが描いた人と異なれば
+			if(globals.drawer != globals.now)
+			{
+				// パスした人を登録し
+				globals.pather.add(next);
+				globals.now = next;
+				// 画面を切り替えて次の人へ
+				Intent intent = new Intent(DrawingActivity.this, DrawingActivity.class);
+				startActivity(intent);
+			}
+			// 描いた人ならば
+			else
+			{
+				// パスした人を全て除外し、
+				globals.pather.clear();
+				// 罰ゲーム通知画面に飛ばす
+				Intent intent = new Intent(DrawingActivity.this, ShowingPenaltyActivity.class);
+				startActivity(intent);
+			}
+		}
+		*/
+	}
+
+	/**
+	 * 入力された文字のバリデーションを行う
+	 */
+	private boolean validation(String str)
+	{
+		final String MATCH_HIRAGANA = "^[\\u3040-\\u309F]+$";
+		// 文字が入力されているか
+		if(str.length() == 0)
+		{
+			Toast.makeText(this, "書く文字を入力して下さい", Toast.LENGTH_SHORT).show();
+			return false;
+		}
+		// ひらがなんみで入力されているか
+		if(!str.matches(MATCH_HIRAGANA))
+		{
+			Toast.makeText(this, "ひらがなのみで入力して下さい", Toast.LENGTH_SHORT).show();
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * 次へボタンを押した時の動作定義
+	 */
+	private void onClickNextButton()
+	{
+		if(!isInputedWord) return;
+		// パスした人が存在するならば
+		if(globals.pather.size() > 0)
+		{
+			// 罰ゲーム通知画面に飛ばす
+			Intent intent = new Intent(DrawingActivity.this, ShowingPenaltyActivity.class);
+			startActivity(intent);
+		}
+		globals.drawer = globals.now;
+		// 順番を次に飛ばして
+		int next = (++globals.now) % globals.player;
+		globals.now = next;
+		// 画面を切り替えて次の人移り、答え予測画面へ
+		Intent intent = new Intent(DrawingActivity.this, GuessActivity.class);
+		startActivity(intent);
+>>>>>>> devel
 	}
 
 	/**
@@ -123,5 +309,29 @@ implements OnClickListener
 	{
 		int r = (int)(Math.random() * TEMPLATE_THEME_WORD.length);
 		return TEMPLATE_THEME_WORD[r];
+<<<<<<< HEAD
+=======
+	}
+
+	/**
+	 *  お題を入力し、エディット出来なくする
+	 */
+	private void disableWordInput()
+	{
+		wordEditText.setText(preWord);
+		wordEditText.setEnabled(false);
+		wordEditText.setFocusable(false);
+
+	}
+
+	/**
+	 *  OKボタンの無効化
+	 */
+	private void disableOkButton()
+	{
+		// ボタンを見えなくする
+		Button okButton = (Button)findViewById(R.id.drawing_word_ok_button);
+		okButton.setVisibility(View.GONE);
+>>>>>>> devel
 	}
 }
--- src/com/example/drawingshiritori/Globals.java
+++ src/com/example/drawingshiritori/Globals.java
@@ -1,26 +1,41 @@
 package com.example.drawingshiritori;
+import java.util.ArrayList;
+
 import android.app.Application;
 
 public class Globals extends Application {
 	//グローバル変数に使用する変数
-	
+
 	//今何番目のプレイヤーか
 	int now;
 	//プレイヤー人数
 	int player;
 	//制限時間
 	int limit;
+<<<<<<< HEAD
 	//何が描かれているのか
 	String word;
 	//絵へのパス
 	String imgPath;
 	
+=======
+	// 描いた人
+	int drawer;
+	// パスした人
+	ArrayList<Integer> pather;
+
+>>>>>>> devel
 	//全て初期化するメソッド
 	public void GlobalAllInit(){
 		now = 1;
 		player = 3;
 		limit = 60;
+<<<<<<< HEAD
 		word = "";
 		imgPath = "";
+=======
+		drawer = 1;
+		pather = new ArrayList<Integer>();
+>>>>>>> devel
 	}
 }
